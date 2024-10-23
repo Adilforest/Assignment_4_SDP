a8078ea9e753eaccf149b2d19cb9f7c22cc5b548
Adil Ormanov <>
2024-10-23 14:38:04 +0500

    commit 0.1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,82 @@
+import interpreter.Interpreter;
+import memento.TextEditorMemento;
+import observer.FirstNewsChannel;
+import observer.NewsAgency;
+import observer.Observer;
+import observer.SecondNewsChannel;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        testExpression();
+        testMemento();
+//        testObserver();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void testExpression() {
+        System.out.println(Interpreter.parse("1+2+3").interpret()); // 6
+        System.out.println(Interpreter.parse("1+2+3-4").interpret()); // 2
+        System.out.println(Interpreter.parse("10+2+3-4-5").interpret()); // 6
+        System.out.println(Interpreter.parse("1 + 2 + 3 + 4").interpret()); // 10
+    }
+
+    public static void testMemento() {
+        TextEditorMemento editor = new TextEditorMemento();
+
+        editor.addText("Hello");
+        editor.save();
+        System.out.println(editor.getText()); // Hello
+
+        editor.addText(" World");
+        editor.save();
+        System.out.println(editor.getText()); // Hello World
+
+        editor.addText("!");
+        editor.save();
+        System.out.println(editor.getText()); // Hello World!
+
+        editor.restore();
+        System.out.println(editor.getText()); // Hello World!
+
+        editor.restore();
+        System.out.println(editor.getText()); // Hello World
+
+        editor.restore();
+        System.out.println(editor.getText()); // Hello
+
+        editor.restore();
+        System.out.println(editor.getText()); //
+
+
+    }
+
+    public static void testObserver() {
+        NewsAgency observable = new NewsAgency();
+        Observer observer1 = new FirstNewsChannel();
+        Observer observer2 = new SecondNewsChannel();
+
+        observable.addObserver(observer1);
+        observable.addObserver(observer2);
+
+        observable.addNews("first news");
+        System.out.println("----");
+
+        observable.addNews("second news");
+        System.out.println("----");
+
+        observable.addNews("third news");
+        System.out.println("----");
+
+        observable.removeObserver(observer1);
+
+        observable.addNews("fourth news");
+        System.out.println("----");
+
+        observable.removeObserver(observer2);
+        observable.addObserver(observer1);
+
+        observable.addNews("fifth news");
+        System.out.println("----");
     }
 }
